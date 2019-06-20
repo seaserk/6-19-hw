cea519483094f009ddcffb8852c1e5c1cbe56a1d
Kyle Seaser <kyleseaser@Kyles-MacBook-Pro.local>
2019-06-19 20:14:31 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -11,3 +11,22 @@ for (i = 1; i <= 20; i++){
 for (i = 2; i <= 200; i +=2){
 	console.log(i);
 }
+
+//3. Write code that logs "Love me, pet me! HSSSSSS!" 20 times. For every even number in your loop, log "...human...why you taking pictures of me?...", 
+//"...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+// COME BACK TO THIS
+//for (i = 1; i <=20; i ++){
+//	console.log ('Love me, pet me! HSSSSSS!')}
+
+// Write a javascript application that logs all numbers from 1 - 100.
+
+// If a number is divisible by 3 log "Fizz" instead of the number.
+
+// If a number is divisible by 5 log "Buzz" instead of the number.
+
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+for (i = 1; i <= 100; i++){
+	console.log(i);
+}
